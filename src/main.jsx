754fb5acf7c7a0ae09e50d8e532a5e0b57a3b46d
dbartolai <drakeab2@illinois.edu>
2025-03-02 13:36:51 -0600

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,8 +5,6 @@ import Sidebar from './Sidebar.jsx'
 import Window from './Window.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <App />
     
-  </StrictMode>
 )
