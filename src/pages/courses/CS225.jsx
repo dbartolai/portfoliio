a4add5d183037bbf90ee3872209c6e100ba31166
dbartolai <drakeab2@illinois.edu>
2025-03-03 23:59:09 -0600

    Finished CS225 Page

--- src/pages/courses/CS225.jsx
+++ src/pages/courses/CS225.jsx
@@ -52,6 +52,56 @@ function Stack(){
     )
 }
 
+function CPP(){
+    return (
+        <>
+            <ul>
+                <li>Learned in depth about memory allocation, classes, and object oriented programming through weekly assignments</li>
+                <li>Use C++ to create proprietary data structures that run on sequences of pixels, working to improve runtimes</li>
+                <li>Solve problems in C++ on biweekly proctored exams</li>
+            </ul>
+        </>
+    )
+}
+
+function Test(){
+    return (
+        <>
+            <ul>
+                <li>Develop Catch2 tests to test my program with small integer values before it runs on images</li>
+                <li>Use breakpoints and print statements to fix segfaults, memory issues, and incorrect outputs </li>
+                <li>Whiteboard simple cases of problems to visualize what is happening at runtime</li>
+            </ul>
+        </>
+    )
+}
+
+function Problem(){
+    return (
+        <>
+            <ul>
+                <li>Solve various puzzles and problems involving developing algorithms each week</li>
+                <li>These problems range from rearranging a linked list in a specific manner to constructing a photomosaic through a BST</li>
+                <li>Take the concrete information from class and utilize it in creative, thoughtful ways to develop a solution</li>
+            </ul>
+        </>
+    )
+}
+
+function Docker(){
+    return (
+        <>
+            <ul>
+                <li>All class operations run in the proprietary Docker container</li>
+                <li>This contains all of the dependencies to run our test cases, and compile correctly for our autograder</li>
+            </ul>
+        </>
+    )
+}
+
+
+
+
 const story = 'Data structures and algorithms is broken down into two key components, each equally important. Theory, and practice. I see theory as the part that was likely very difficult to come up with, yet easy for the student to understand. This is why whiteboarding is so helpful when programming. It\'s not that difficult to see how different pointers should move in different cases. Practice, on the other hand, makes up the majority of the time commitment of this class. We implement the data structures with respect to sequences of pixels, which can make testing and debugging very difficult. Unfortunately, I don\'t believe I can share any of the code I write for this class.'
 
 const data_map = {
@@ -81,6 +131,33 @@ const data_map = {
     }
 }
 
+const skills_map = {
+    'C++' : {
+        'title' : 'C++',
+        'subtitle' : '',
+        'description' : <CPP/>,
+        'color' : 'grey'
+    },
+    'Testing' : {
+        'title' : 'Testing & Debugging',
+        'subtitle' : '',
+        'description' : <Test />,
+        'color' : 'green'
+    },
+    'ProblemSolving' : {
+        'title' : 'Problem Solving',
+        'subtitle' : '',
+        'description' : <Problem />,
+        'color' : 'yellow'
+    },
+    'Docker' : {
+        'title' : 'Docker Container',
+        'subtitle' : '',
+        'description' : <Docker/>,
+        'color' : 'orange'
+    }
+}
+
 function CS225(){
     return(
         <>
@@ -88,7 +165,8 @@ function CS225(){
             <h1 className='story-header'>Theory vs Practice:</h1>
             <p className='story'>{story}</p>
         </div>
-        <Group group_title={'Data Structures'} module_map={data_map}/>
+        <Group group_title={'Data Structures:'} module_map={data_map}/>
+        <Group group_title={'Skills'} module_map={skills_map}/>
         </>
     )
 }
