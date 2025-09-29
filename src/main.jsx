299a2f07ff6ba3231c158503479e30e9780d1017
dbartolai <drakeab2@illinois.edu>
2025-09-28 20:57:21 -0500

    routing logic fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,27 @@ import App from './App.jsx'
 import Blog from './Blog.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function RestoreRedirect() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = sessionStorage.getItem('redirectPath');
+    if (path) {
+      sessionStorage.removeItem('redirectPath');
+      navigate(path, { replace: true });
+    }
+  }, [navigate]);
+
+  return null;
+}
+
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <RestoreRedirect/>
     <Routes>
     <Route path='/' element={<App/>}/>
     <Route path="/blog" element={<Blog/>}/>
